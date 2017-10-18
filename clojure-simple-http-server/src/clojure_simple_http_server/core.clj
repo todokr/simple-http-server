9a4f17ff35f82ea7b7465b81522b2e7680f2fca1
todokr <shunsuke.tadokoro@bizreach.co.jp>
2017-10-19 00:20:08 +0900

    fix

--- clojure-simple-http-server/src/clojure_simple_http_server/core.clj
+++ clojure-simple-http-server/src/clojure_simple_http_server/core.clj
@@ -1,8 +1,9 @@
 (ns clojure-simple-http-server.core
   (:gen-class)
-  (:require [clojure-simple-http-server.utils.config-loader :refer [load-config]]))
+  (:require [clojure-simple-http-server.utils.config-loader :refer [load-config]]
+            [clojure-simple-http-server.server :as server]))
 
 (defn -main [& args]
   (let [config (load-config)]
-    (web/run handler config)
+    (server/run! handler config)
     (println "Hello, World!")))
--- clojure-simple-http-server/src/clojure_simple_http_server/server.clj
+++ clojure-simple-http-server/src/clojure_simple_http_server/server.clj
@@ -1,12 +1,15 @@
 (ns clojure-simple-http-server.server
-  (:require [clojure.java.io :as io])
+  (:require [clojure.java.io :as io]
+            [clojure.core.async :refer [thread]]
+            [clojure-simple-http-server.request-parser :as parser]
+            [clojure-simple-http-server.request-handler :as handler]
+            [clojure-simple-http-server.response-writer :as writer])
   (:import (java.net InetSocketAddress ServerSocket)))
 
 (defn run!
   "Runs `handler` with given `config`"
-  [handler config]
-  (let [conf (select-keys config [:host :port :backlog :error-pages])
-        address (new InetSocketAddress (:host conf))
+  [handler {:keys [host port backlog error-pages] :as config}]
+  (let [address (new InetSocketAddress host)
         server-socket (new ServerSocket)]
     (.bind server-socket address backlog)
     (prn (str "Server Started: " (.getInetAddress server-socket) ":" (.getLocalPort server-socket)))
@@ -14,8 +17,8 @@
     (while true
       (let [socket (.accept server-socket)]
         (thread
-          (with-open [out (io/output-stream (.getOutputStream))
-                      in (io/reader (.getInputStream))]
+          (with-open [out (io/output-stream (.getOutputStream socket))
+                      in (io/reader (.getInputStream socket))]
             (-> (parser/parse-request (slurp in))
-                (handler/handle-request (:error-pages conf))
-                (write out))))))))
+                (handler/handle-request error-pages)
+                (writer/write out))))))))
