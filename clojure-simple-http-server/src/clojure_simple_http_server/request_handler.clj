f74379faa237894c7e66f9eafe3b69a6227727c1
todokr <shunsuke.tadokoro@bizreach.co.jp>
2017-10-19 09:32:06 +0900

    mock handler

--- clojure-simple-http-server/src/clojure_simple_http_server/request_handler.clj
+++ clojure-simple-http-server/src/clojure_simple_http_server/request_handler.clj
@@ -1,7 +1,10 @@
-(ns clojure-simple-http-server.request-handler)
+(ns clojure-simple-http-server.request-handler
+  (:import (java.time OffsetDateTime ZoneOffset)))
 
 (defn handle-request [req error-pages]
   {:status 200
    :reason-phrase "OK"
+   :date (. OffsetDateTime (now ZoneOffset/UTC))
+   :last-modified (. OffsetDateTime (now ZoneOffset/UTC))
    :content-type "text/html"
    :body (.getBytes "Hello Server!")})
