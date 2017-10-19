0912b38e6a563e808f50b7d8877ee905c6e3e918
todokr <shunsuke.tadokoro@bizreach.co.jp>
2017-10-19 09:19:29 +0900

    fix response writer

--- clojure-simple-http-server/src/clojure_simple_http_server/response_writer.clj
+++ clojure-simple-http-server/src/clojure_simple_http_server/response_writer.clj
@@ -1,14 +1,18 @@
 (ns clojure-simple-http-server.response-writer
   (:require [clojure-simple-http-server.utils.config-loader]
-            [clojure-simple-http-server.utils.http-token :refer [CRLF]]))
+            [clojure-simple-http-server.utils.http-token :refer [CRLF SP]])
+  (:import (java.time.format DateTimeFormatter)))
 
-(defn write [res out]
+(defn write [{:keys [status reason-phrase date content-type last-modified body]} out]
   (let [header (str
-                "HTTP/1.1 200 OK" CRLF
-                "Content-Length: " (count (:body res)) CRLF
-                "Content-Type: " (:mime (:content-type res)) CRLF
-                "Connection: " (or (:connection res) "Close") CRLF
+                "HTTP/1.1" SP status SP reason-phrase CRLF
+                "Date:" (.format date DateTimeFormatter/RFC_1123_DATE_TIME)
+                "Content-Length:" (count body) CRLF
+                "Content-Type:" content-type CRLF
+                "Connection: Close" CRLF
+                (when last-modified
+                  "Last-Modified:" (.format last-modified DateTimeFormatter/RFC_1123_DATE_TIME))
                 CRLF)]
     (.write out (.getBytes header))
-    (.write out (:body res))
+    (.write out body)
     (.write out (.getBytes CRLF))))
