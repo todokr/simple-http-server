09171b69f1975550ef9a6fa8e7032ddf38710055
todokr <shunsuke.tadokoro@bizreach.co.jp>
2017-11-12 00:54:36 +0900

    normalize request path

--- clojure-simple-http-server/src/clojure_simple_http_server/request_handler.clj
+++ clojure-simple-http-server/src/clojure_simple_http_server/request_handler.clj
@@ -1,5 +1,30 @@
 (ns clojure-simple-http-server.request-handler
-  (:import (java.time OffsetDateTime ZoneOffset)))
+  (:require [clojure.string :as s]
+            [clojure.java.io :as io])
+  (:import (java.time OffsetDateTime ZoneOffset)
+           (java.nio.file Paths Files)))
+
+(defn normalize-path [str]
+  (let [path (-> (Paths/get "public" (into-array [str]))
+                 (.normalize)
+                 (io/as-file))]
+    (if (.isDirectory path)
+      (io/file path "index.html")
+      path)))
+
+(defn dispatch [req]
+  (cond
+    (nil? req) ::bad-request
+    (not (.startsWith (normalize-path (:url req)) public-dir-path)) ::forbidden
+    :else ::found))
+
+(defmulti handle-request dispatch)
+
+(defmethod handle-request ::forbidden [req]
+  (str "forbidden: " req))
+
+(defmethod handle-request ::not-found [req]
+  (str "not-found: " req))
 
 (defn handle-request [req error-pages]
   {:status 200
