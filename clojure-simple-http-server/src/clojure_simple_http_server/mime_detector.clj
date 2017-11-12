d8843788d37818055d3f18c1fc484e6fc897b51f
todokr <shunsuke.tadokoro@bizreach.co.jp>
2017-11-13 00:47:09 +0900

    detect mime

---
+++ clojure-simple-http-server/src/clojure_simple_http_server/mime_detector.clj
@@ -0,0 +1,11 @@
+(ns clojure-simple-http-server.mime-detector
+  (:require [clojure.edn :as edn]
+            [clojure.string :as s]
+            [clojure.java.io :as io]))
+
+(defn detect [path]
+  (let [ext (last (s/split (str path) #"\."))]
+    (-> (io/resource "mimes.edn")
+      (slurp)
+      (edn/read-string)
+      (get (keyword ext) "application/octet-stream"))))
--- clojure-simple-http-server/src/clojure_simple_http_server/request_handler.clj
+++ clojure-simple-http-server/src/clojure_simple_http_server/request_handler.clj
@@ -4,46 +4,51 @@
   (:import (java.time OffsetDateTime ZoneOffset)
            (java.nio.file Paths Path Files LinkOption)))
 
-; (def test-path (Paths/get "public/index.html" (into-array String nil)))
+(defn- exists? [^Path path]
+  (Files/exists path (into-array LinkOption nil)))
 
-(defn- ^Path normalize-path [^String url]
-  (if-let [path-str url]
-   (let [path (.normalize (Paths/get "public" (into-array [url])))]
-    (if (Files/isDirectory path (into-array LinkOption nil))
-      (.resolve path "index.html")
-      path))
-   nil))
+(defn- resolve-public [path-str]
+  (Paths/get "public" (into-array [path-str])))
 
 (defn- path->bytes [^Path path]
   (with-open [stream (io/input-stream (.toFile path))]
     (byte-array (take-while #(not= -1 %) (repeatedly #(.read stream))))))
 
-(defn- handle-found [path]
-  (let [body (path->bytes path)]
-    {:status 200
-     :content-type "text/html"
-     :content-length (count body)
-     :body body}))
-
-(defn- resolve [path]
-  (cond
-    (nil? path) {:status 400}
-    (not (.startsWith path public-dir-path)) {:status 403}
-    (not (Files/exists path (into-array LinkOption nil))) {:status 404}
-    :else (handle-found path)))
+(defn- ^Path normalize-path [^String url]
+  (if-let [path-str url]
+   (let [path (.normalize (resolve-public path-str))]
+    (if (Files/isDirectory path (into-array LinkOption nil))
+      (.resolve path "index.html")
+      path))
+   nil))
 
 (def reason-phrases {200 "OK"
                      400 "BadRequest"
                      403 "Forbidden"
                      404 "NotFound"})
 
-(defn- assoc-reason [res]
-  (assoc res :reason-phrase (reason-phrases (:status res))))
+(defn- handle-ok [path]
+  (let [body (path->bytes path)]
+    {:status 200
+     :reason-phrase (reason-phrases 200)
+     :content-type "text/html"
+     :content-length (count body)
+     :body body}))
+
+(defn- handle-not-ok [status]
+  {:status status
+   :reason-phrase (reason-phrases status)
+   :content-type "text/html"
+   :body (path->bytes (resolve-public (str status ".html")))})
+
+(defn- resolve [path]
+  (cond
+    (nil? path) (handle-not-ok 400)
+    (not (.startsWith path public-dir-path)) (handle-not-ok 403)
+    (not (exists? path)) (handle-not-ok 404)
+    :else (handle-ok path)))
 
 (defn handle-request [req]
   (-> (:url req)
       (normalize-path)
-      (resolve)
-      (assoc-reason)))
-
-; (handle-request {:url "/index.html"})
+      (resolve)))
