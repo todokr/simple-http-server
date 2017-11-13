c13d7eb3be776fd2e2682410d7266c36b4250f9d
todokr <shunsuke.tadokoro@bizreach.co.jp>
2017-11-13 10:08:56 +0900

    impl request parser

--- clojure-simple-http-server/src/clojure_simple_http_server/request_parser.clj
+++ clojure-simple-http-server/src/clojure_simple_http_server/request_parser.clj
@@ -1,6 +1,13 @@
-(ns clojure-simple-http-server.request-parser)
+(ns clojure-simple-http-server.request-parser
+  (:require [clojure.java.io :as io]
+            [clojure.string :as s]))
 
-(defn parse-request [req]
-  {:method "GET"
-   :path "/"
-   :http-version "HTTP1.1"})
+(defn from-input-stream [in]
+  (let [[method target-path http-version]
+        (-> (slurp in)
+            (s/split-lines)
+            (first)
+            (s/split #"\s"))]
+    {:method method
+     :target-path target-path
+     :http-version http-version}))
