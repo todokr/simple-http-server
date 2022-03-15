78e46bfb917ef5291bc3a1fd37f96898ea9b8edd
Shunsuke Tadokoro <shunsuke.tadokoro@bizreach.co.jp>
2022-03-15 15:11:24 +0900

    update clojure version

--- clojure-simple-http-server/project.clj
+++ clojure-simple-http-server/project.clj
@@ -2,7 +2,7 @@
   :description "Simple HTTP Server written in Clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.2"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/core.async "1.3.610"]]
   :main ^:skip-aot clojure-simple-http-server.core
   :target-path "target/%s"
